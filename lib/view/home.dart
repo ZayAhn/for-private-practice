8372313d397f711e5af75abbc4d68b31f7bcb643
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-05-11 23:12:59 +0900

    add,fixed : needed fix the double button and minus button

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
+// import 'package:jaewoo/viewmodel/practice.dart/';
+
 
 final counterProvider = StateProvider((ref) => 0);
 
@@ -9,6 +11,17 @@ class Home extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter'),
+        actions: [
+          IconButton(
+              onPressed: () => ref.read(counterProvider.state).state++,
+              icon: Text('plus')),
+          IconButton(
+              onPressed: () => ref.read(counterProvider.state).state * 2,
+              icon: Text('double')),
+          IconButton(
+              onPressed: () => ref.read(counterProvider.state).state-- print,
+              icon: Text('minus'))
+        ],
       ),
       body: Center(
         child: Consumer(builder: (context, ref, _) {
